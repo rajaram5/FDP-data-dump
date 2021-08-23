11967df17bdc21225fb94dc353fd61d06876bfee
Rajaram Kaliyaperumal <rr.kaliyaperumal@gmail.com>
2021-08-23 10:14:39 +0200

    Example fdp url changed

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import FDPClientLDPBased as fdpLdpClient
 
-fdp_url = "http://lumc-beat-covid.fair-dtls.surf-hosted.nl"
+fdp_url = "https://ejprd.fair-dtls.surf-hosted.nl/wp13-fdp"
 fdp = fdpLdpClient.FdpLdpClient(fdp_url)
 fdp.create_fdp_data_dump()
